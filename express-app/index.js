dac2674fe5cf9bfaf8e16fb6ee4181363ae3cfaf
tankazuki <34229874+tankazuki@users.noreply.github.com>
2019-02-11 17:32:24 +0900

    [Add] ページ処理を記述

--- express-app/index.js
+++ express-app/index.js
@@ -1,10 +1,17 @@
 let express = require('express');
+let ejs = require('ejs');
+
 let app = express();
 
+app.engine('ejs', ejs.renderFile);
+
 app.get('/', (req, res) => {
-  res.send('Welcomet to Express!');
+    res.render('index.ejs', {
+        title: 'Index',
+        content: 'This is Express-app Top Page'
+    });
 });
 
 app.listen(3000, () => {
-   console.log('Start server port:3000');
+   console.log('Server is running!');
 });
