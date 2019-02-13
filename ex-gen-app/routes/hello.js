a517e2584475b2779f007d1d8dc5e2cd78850e01
tankazuki <34229874+tankazuki@users.noreply.github.com>
2019-02-13 12:40:36 +0900

    [Add] クエリパラメータの値を受け取れるように処理

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -2,10 +2,12 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', (req, res, next) => {
+    let name = req.query.name;
+    let mail = req.query.mail;
    let data = {
        title: 'Hello!',
-       content: `これは、サンプルのコンテンツです。
-                 this is sample content.`
+       content: `あなたの名前は「${name}」。
+                 メールアドレスは「${mail}」です。`
    };
    res.render('hello', data);
 });
